7ecefc3605d05b2a3f324b2dd6e8863237502a39
Rok <rexblanegamer@gmail.com>
2023-01-19 04:43:28 -0300

    Remaking Navbar, cards and creating lang dropbox

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const logo = document.querySelector("#navbar_logo")
+const navbar = document.querySelector(".navbar")
 const items = document.querySelectorAll(".navbar_item")
+
+const lang = document.querySelector("#dropbtn")
+
 
 window.addEventListener('scroll', function(){
   var scroll = $(window).scrollTop(),
       dheight = $(document).height(),
       wheight = $(window).height();
 
-  var scrollPercent = (scroll / (dheight - wheight)) * 100;
-
-  if (scrollPercent > 5) {
+  //var scrollPercent = (scroll / (dheight - wheight)) * 100;
+  if (scroll > 80) {
     logo.id = "navbar_logo_scroll";
   } else {
     logo.id = "navbar_logo";
   }
-  
-
-  console.clear();
-  console.log(scrollPercent);
 })
-
